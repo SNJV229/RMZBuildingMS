5a3030f6b57fc45aa0770b5d279b7bbe9557a734
Sanjeev Kumar <sk6208880@gmail.com>
2022-10-18 10:12:38 +0530

    Added Unit Test

--- RMZBuildingMS/Repository/GetInfo.cs
+++ RMZBuildingMS/Repository/GetInfo.cs
@@ -28,25 +28,30 @@ namespace RMZBuildingMS.Repository
                 return result;
             }
 
-            //else if( levelOfInformation == "Building")
-            //{
-            //    var result = _context.buildings.Where(x=> x.)
-            //    .Join(_context.floors, z => z.Floor.FloorId, f => f.FloorId, (z, f) => new { f, z })
-            //    .Join(_context.buildings, bf => bf.f.BuildingId, b => b.BuildingId, (b, bf) => new
-            //    {
-            //        facilityName = bf.Facility.FacilityName,
-            //        buildingName = bf.BuildingName,
-            //        floorName = b.f.FloorName,
-            //        zoneName = b.z.ZoneId,
-            //        zoneElectricreading = b.z.ElectricMeter,
-            //        zoneWaterreading = b.z.WaterMeter
-            //    });
-            //    return result;
+            else if (levelOfInformation == "Building")
+            {
+                var result = _context.zones.Where(x => x.ReadingDate >= startDate && x.ReadingDate <= endDate)
+                    .Join(_context.floors, z => z.Floor.FloorId, f => f.FloorId, (z, f) => new { f, z })
+                    .Join(_context.buildings, bz => bz.z.BuildingId, bu => bu.BuildingId, (bu, bz) => new
+                    {
+                        buildingName = bz.BuildingName,
+                        electricityBill = bu.z.ElectricMeter,
+                        waterBill = bu.z.WaterMeter
+                    });
+                return result;
 
-            //}
+
+            }
             else
             {
-                var result = _context.zones;
+                var result = _context.zones.Where(x => x.ReadingDate >= startDate && x.ReadingDate <= endDate)
+                    .Join(_context.floors, z => z.Floor.FloorId, f => f.FloorId, (z, f) => new { f, z })
+                    .Join(_context.buildings, bz => bz.z.BuildingId, bu => bu.BuildingId, (bu, bz) => new
+                    {
+                        facilityName = bu.z.Facility.FacilityName,
+                        electricityBill = bu.z.ElectricMeter,
+                        waterBill = bu.z.WaterMeter,
+                    });
                 return result;
             }
         }
---
+++ RMZBuildingTest/RMZTest.cs
@@ -0,0 +1,50 @@
+using RMZBuildingMS.Controllers;
+using RMZBuildingMS.Repository;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+using RMZBuildingMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RMZBuildingTest
+{
+    public class RMZTest
+    {
+        private readonly IGetInfo _iGetInfo;
+        private readonly RMZController _rmzController;
+        private readonly RMZContext _context;
+
+
+        public RMZTest()
+        {
+            _iGetInfo = A.Fake<IGetInfo>();
+            _rmzController = new RMZController(_context, _iGetInfo);
+        }
+
+        [Fact]
+        public void rmzcontroller_getAtLevel_Returnok()
+        {
+            string facilitylevel = "Zone";
+            DateTime startDate = new DateTime(2022,06,10);
+            DateTime EndDate = new DateTime(2022, 08, 13);
+            string zoneid = "Zone6";
+            var obj = new
+            {
+                facilityName = "WhiteField",
+                buildingName = "Building2",
+                floorName = "Floor2",
+                zoneName = "Zone6",
+                zoneElectricreading = 3549.0,
+                zoneWaterreading = 268.0
+            };
+
+            A.CallTo(() => _iGetInfo.getAtLevel(A<string>.Ignored, A<DateTime>.Ignored, A<DateTime>.Ignored)).Returns(obj);
+
+            var result = _rmzController.GetActionResult(facilitylevel, startDate, EndDate);
+
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+    }
+}
+
+
